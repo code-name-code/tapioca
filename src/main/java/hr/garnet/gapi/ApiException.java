3c909f5bf802879f182107a3002771c4b09f328a
vsmid <vedransmid@gmail.com>
2022-12-30 07:52:56 +0100

    Added new constructor with only status and message to ApiException

--- src/main/java/hr/garnet/gapi/ApiException.java
+++ src/main/java/hr/garnet/gapi/ApiException.java
@@ -1,9 +1,10 @@
 package hr.garnet.gapi;
 
 /**
- * {@link ApiServlet} knows how to handle these kind of exceptions. If there is no global exception
- * handler defined and {@link ApiException} is thrown inside {@link ApiCommand}, {@link ApiServlet}
- * will write status and message received from the caught exception to the response output stream.
+ * {@link hr.garnet.gapi.internal.ApiServlet} knows how to handle these kind of exceptions. If there
+ * is no global exception handler defined and {@link ApiException} is thrown inside {@link
+ * ApiCommand}, {@link hr.garnet.gapi.internal.ApiServlet} will write status and message received
+ * from the caught {@link ApiException} to the response output stream.
  *
  * @author vedransmid@gmail.com
  */
@@ -22,6 +23,11 @@ public class ApiException extends RuntimeException {
 
   public ApiException(String message, Throwable cause, int status) {
     super(message, cause);
+    this.status = status;
+  }
+
+  public ApiException(int status, String message) {
+    super(message);
     this.status = status;
   }
 
