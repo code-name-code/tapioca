7d02f13c782f5e652d45159c9260e1ad09828608
vsmid <vedransmid@gmail.com>
2023-01-25 16:24:55 +0100

    Processor will now try to delegate some request processing to default HttpServlet#service implementation

--- src/main/java/hr/codenamecode/tapioca/internal/Processor.java
+++ src/main/java/hr/codenamecode/tapioca/internal/Processor.java
@@ -3,18 +3,19 @@ package hr.codenamecode.tapioca.internal;
 import hr.codenamecode.tapioca.Response;
 import hr.codenamecode.tapioca.ApiException;
 import hr.codenamecode.tapioca.Bindings;
+import hr.codenamecode.tapioca.ExceptionHandler;
 import hr.codenamecode.tapioca.Request;
 import hr.codenamecode.tapioca.RequestHandler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import static jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
-import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
-import static jakarta.servlet.http.HttpServletResponse.SC_NOT_IMPLEMENTED;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author vedransmid@gmail.com
@@ -29,90 +30,101 @@ public class Processor extends HttpServlet {
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    Map<String, RequestHandlerHolder> requestHandlerMappings =
-        switch (req.getMethod()) {
-          case "GET" -> apiConfigurer.getGetMapping();
-          case "POST" -> apiConfigurer.getPostMapping();
-          case "PUT" -> apiConfigurer.getPutMapping();
-          case "DELETE" -> apiConfigurer.getDeleteMapping();
-          case "TRACE" -> apiConfigurer.getTraceMapping();
-          case "HEAD" -> apiConfigurer.getHeadMapping();
-          case "OPTIONS" -> apiConfigurer.getOptionsMapping();
-          default -> null;
-        };
+    Map<String, RequestHandlerHolder> requestHandlers = getRequestHandlerMappings(req.getMethod());
 
-    if (requestHandlerMappings == null) {
-      resp.setStatus(SC_NOT_IMPLEMENTED);
-      return;
+    if (requestHandlers == null || requestHandlers.isEmpty()) {
+      try {
+        super.service(req, resp);
+        return;
+      } catch (ServletException | IOException ex) {
+        return;
+      }
     }
 
-    boolean mappingFound = false;
-    for (String mapping : requestHandlerMappings.keySet()) {
-      String matchedValue = Request.getMatchedValue(req.getHttpServletMapping());
-      if (matchedValue.matches(mapping)) {
-        Request apiReq = new Request(req, mapping);
-        Response apiResp = new Response(resp);
-        RequestHandlerHolder requestHandlerHolder = requestHandlerMappings.get(mapping);
-        RequestHandler method;
-        mappingFound = true;
+    String matchedMapping = getMatchedMapping(requestHandlers, req.getHttpServletMapping());
+
+    if (matchedMapping == null) {
+      try {
+        super.service(req, resp);
+        return;
+      } catch (ServletException | IOException ex) {
+        return;
+      }
+    }
+
+    Request apiReq = new Request(req, matchedMapping);
+    Response apiResp = new Response(resp);
+    RequestHandlerHolder requestHandlerHolder = requestHandlers.get(matchedMapping);
+    RequestHandler method;
+
+    try {
+      if (requestHandlerHolder.containsImplementation()) {
+        method = requestHandlerHolder.getRequestHandlerImpl();
+      } else {
+        method =
+            Bindings.getRequestHandlerFactory()
+                .apply(requestHandlerHolder.getRequestHandlerClass());
+      }
+      method.setServletConfig(getServletConfig());
+      method.handle(apiReq, apiResp);
+      if (!resp.isCommitted()) {
+        resp.flushBuffer();
+      }
+    } catch (Exception e) {
+      Optional<ExceptionHandler> exceptionHandler = Bindings.getExceptionHandler();
+
+      if (exceptionHandler.isPresent()) {
+        exceptionHandler.get().handleException(e, apiReq, apiResp);
         try {
-          if (requestHandlerHolder.containsImplementation()) {
-            method = requestHandlerHolder.getRequestHandlerImpl();
-          } else {
-            method =
-                Bindings.getRequestHandlerFactory()
-                    .apply(requestHandlerHolder.getRequestHandlerClass());
-          }
-          method.setServletConfig(getServletConfig());
-          method.handle(apiReq, apiResp);
           if (!resp.isCommitted()) {
             resp.flushBuffer();
           }
-        } catch (Exception e) {
-          Bindings.getExceptionHandler()
-              .ifPresentOrElse(
-                  eh -> {
-                    eh.handleException(e, apiReq, apiResp);
-                    try {
-                      if (!resp.isCommitted()) {
-                        resp.flushBuffer();
-                      }
-                    } catch (IOException ignored) {
+        } catch (IOException ignored) {
 
-                    }
-                  },
-                  () -> {
-                    try {
-                      if (!resp.isCommitted()) {
-                        if (e instanceof ApiException apiException) {
-                          resp.setStatus(apiException.getStatus());
-                          if (Objects.nonNull(apiException.getMessage())) {
-                            resp.getWriter().append(apiException.getMessage());
-                          }
-                        } else {
-                          resp.setStatus(SC_INTERNAL_SERVER_ERROR);
-                        }
-                        resp.flushBuffer();
-                      }
-                    } catch (IOException ignored) {
-
-                    }
-                  });
-          return;
-        }
-      }
-    }
-
-    if (!mappingFound) {
-      if ("OPTIONS".equals(req.getMethod())) {
-        try {
-          doOptions(req, resp);
-        } catch (ServletException | IOException ex) {
-          resp.setStatus(SC_INTERNAL_SERVER_ERROR);
         }
       } else {
-        resp.setStatus(SC_NOT_FOUND);
+        try {
+          if (!resp.isCommitted()) {
+            if (e instanceof ApiException apiException) {
+              resp.setStatus(apiException.getStatus());
+              if (Objects.nonNull(apiException.getMessage())) {
+                resp.getWriter().append(apiException.getMessage());
+              }
+            } else {
+              resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+            }
+            resp.flushBuffer();
+          }
+        } catch (IOException ignored) {
+
+        }
       }
     }
+  }
+
+  private Map<String, RequestHandlerHolder> getRequestHandlerMappings(String httpMethod) {
+    return switch (httpMethod) {
+      case "GET" -> apiConfigurer.getGetMapping();
+      case "POST" -> apiConfigurer.getPostMapping();
+      case "PUT" -> apiConfigurer.getPutMapping();
+      case "DELETE" -> apiConfigurer.getDeleteMapping();
+      case "TRACE" -> apiConfigurer.getTraceMapping();
+      case "HEAD" -> apiConfigurer.getHeadMapping();
+      case "OPTIONS" -> apiConfigurer.getOptionsMapping();
+      default -> null;
+    };
+  }
+
+  private String getMatchedMapping(
+      Map<String, RequestHandlerHolder> requestHandlers, HttpServletMapping httpServletMapping) {
+
+    for (String mapping : requestHandlers.keySet()) {
+      String matchedValue = Request.getMatchedValue(httpServletMapping);
+      if (matchedValue != null && matchedValue.matches(mapping)) {
+        return mapping;
+      }
+    }
+
+    return null;
   }
 }
